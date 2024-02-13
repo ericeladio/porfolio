a4863219fb8a873fa5bbce970efd80f0a5c61010
Eric <diego123@Eric.local>
2024-02-13 16:02:03 -0600

    dinamig timeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
 
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-      mode: 'standalone',
-  }),
   i18n: {
-      defaultLocale: "en",
-      locales: ["en", "es",],
-      routing: {
-          prefixDefaultLocale: false
-      }
-    },
-});
+    defaultLocale: "en",
+    locales: ["en", "es"]
+  }
+})
--- public/content.js
+++ public/content.js
@@ -1,30 +1,71 @@
-export const INFO ={
-    english:{
-        nav:{
-            experience:"Experience",
-            projects:"Projects",
-            about_me:"About me",
-            contact:"Contact",
-            lang:"Español",
+export const INFO = {
+    english: {
+        nav: {
+            experience: "Experience",
+            projects: "Projects",
+            about_me: "About me",
+            contact: "Contact",
+            lang: "Español",
         },
-        intro:{
-            based:'Developer, based in Mexico.',
-            slogan:'+ 3 years of experience, lover of good practices and clean code.',
+        intro: {
+            based: 'Developer, based in Mexico.',
+            slogan: '+ 3 years of experience, lover of good practices and clean code.',
+        },
+        work_experience: {
+            work: [
+                {
+                    title: 'SEGARMEX',
+                    description: 'dfdfsdfsd',
+                },
+                {
+                    title: 'SEGARMEX',
+                    description: 'dfdfsdfsd',
+                },
+                {
+                    title: 'SEGARMEX',
+                    description: 'dfdfsdfsd',
+                },
+                {
+                    title: 'SEGARMEX',
+                    description: 'dfdfsdfsd',
+                },
+            ]
+
         },
 
     },
-    spanish:{
-        nav:{
-            experience:"Experiencia",
-            projects:"Projectos",
-            about_me:"Sobre mi",
-            contact:"Contacto",
-            lang:"English",
+    spanish: {
+        nav: {
+            experience: "Experiencia",
+            projects: "Projectos",
+            about_me: "Sobre mi",
+            contact: "Contacto",
+            lang: "English",
         },
-        intro:{
-            based:'Desarrollador, ubicado en Mexico.',
-            slogan:'más de 3 años de experiencia, amante de las buenas practicas y el código limpio.',
+        intro: {
+            based: 'Desarrollador, ubicado en Mexico.',
+            slogan: 'más de 3 años de experiencia, amante de las buenas practicas y el código limpio.',
         },
+        work_experience: {
+            work: [
+                {
+                    title: 'SEGARMEX',
+                    description: 'dfdfsdfsd',
+                },
+                {
+                    title: 'SEGARMEX',
+                    description: 'dfdfsdfsd',
+                },
+                {
+                    title: 'SEGARMEX',
+                    description: 'dfdfsdfsd',
+                },
+                {
+                    title: 'SEGARMEX',
+                    description: 'dfdfsdfsd',
+                },
+            ]
+        }
 
     }
 }
