a25d3d67be10e87db6268b5d206daf7c65fe4c2e
Eric <diego123@Eric.local>
2024-02-15 15:55:06 -0600

    help me please

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from "astro/config"
-import node from '@astrojs/node';
 
 export default defineConfig({
   site: 'https://ericeladio.github.io',
   base: '/https://github.com/ericeladio/porfolio',
-  output: 'server',
-    adapter: node({
-        mode: 'standalone',
-    }),
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"]
