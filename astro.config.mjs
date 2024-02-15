486c64fb9b4ae7eada4eeb9443ea4903efcdd14b
Eric <diego123@Eric.local>
2024-02-15 13:50:56 -0600

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config"
 
 export default defineConfig({
   site: 'https://ericeladio.github.io',
+  base: '/https://github.com/ericeladio/porfolio',
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"]
