22e014889d0f7926b3b26404b83360161e2b4c5e
Eric <diego123@Eric.local>
2024-02-15 16:23:39 -0600

    çreate css sheet file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config"
 
 export default defineConfig({
   site: 'https://ericeladio.github.io',
-  base: '/https://github.com/ericeladio/porfolio',
+  base: '/porfolio',
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"]
